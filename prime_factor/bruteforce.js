132bfbdd2e51ed834e25b3055b5a25e21406d61d
anshit-rangra <anshit22rangra@gmail.com>
2026-03-30 21:12:44 +0530

    Prime factor

--- prime_factor/bruteforce.js
+++ prime_factor/bruteforce.js
@@ -5,7 +5,7 @@ function primeFactor(n) {
 
     let i = 2;
 
-    while(i <= Math.floor(n/2)){ // optimised condition( i * i <= n) || i <= n/2
+    while(i*i <= n){ // optimised condition( i * i <= n) || i <= n/2
 
         while(n % i === 0){
             arr.push(i)
@@ -15,8 +15,12 @@ function primeFactor(n) {
         i++
     }
 
+    if(n>1){
+        arr.push(i)
+    }
+
     return arr;
 
 }
 
-console.log(primeFactor(18))
+console.log(primeFactor(10))
