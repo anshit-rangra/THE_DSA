634de6bb5df4439e8c5e2a015f38d327bea16170
anshit-rangra <anshit22rangra@gmail.com>
2026-05-03 19:36:56 +0530

    Triangle pattern code

---
+++ patterns/IRtriangle.js
@@ -0,0 +1,11 @@
+function printInvertedRightTriangle(n) {
+    // Write your logic here
+    for(let i = n; i>=1; i--){
+        for(let j = i; j>=1; j--){
+            process.stdout.write(`* `)
+        }
+        console.log()
+    }
+}
+
+printInvertedRightTriangle()
---
+++ patterns/Rtriangle.js
@@ -0,0 +1,12 @@
+
+function printRightTriangle(n) {
+    // Write your logic here
+    for(let i = 1; i<=n; i++){
+        for(let j = 1; j<=i; j++){
+            process.stdout.write(`* `)
+        }
+        console.log()
+    }
+}
+
+printRightTriangle();
